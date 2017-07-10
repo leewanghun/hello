e6982fb3c8211edcbe622f7b449e6b78c7ba98b2
ken <ken@ken>
2017-07-10 17:08:52 +0700

    add code Spring MVC Interceptor.

--- SpringMVCStaticResource/src/main/java/com/test/springmvcresource/config/WebMvcConfig.java
+++ SpringMVCStaticResource/src/main/java/com/test/springmvcresource/config/WebMvcConfig.java
@@ -3,8 +3,13 @@ package com.test.springmvcresource.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.test.springmvcinterceptor.interceptor.AdminInterceptor;
+import com.test.springmvcinterceptor.interceptor.LogInterceptor;
+import com.test.springmvcinterceptor.interceptor.OldLoginInterceptor;
  
  
 @Configuration
@@ -28,4 +33,20 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
        configurer.enable();
    }
  
+   @Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		// LogInterceptor áp dụng cho mọi URL.
+		registry.addInterceptor(new LogInterceptor());
+
+		// Đường dẫn login cũ, không còn sử dụng nữa.
+		// Sử dụng OldURLInterceptor để điều hướng tới một URL mới.
+		registry.addInterceptor(new OldLoginInterceptor())//
+				.addPathPatterns("/admin/oldLogin");	
+
+		// Interceptor này áp dụng cho các URL có dạng /admin/*
+		// Loại đi trường hợp /admin/oldLogin
+		registry.addInterceptor(new AdminInterceptor())//
+				.addPathPatterns("/admin/*")//
+				.excludePathPatterns("/admin/oldLogin");
+	}
 }
--- SpringMVCStaticResource/src/main/java/com/test/springmvcresource/controller/MainController.java
+++ SpringMVCStaticResource/src/main/java/com/test/springmvcresource/controller/MainController.java
@@ -11,5 +11,25 @@ public class MainController {
     public String staticResource(Model model) {
         return "staticResourceTest";
     }
-     
+    
+    // Đường dẫn này không còn sử dụng nữa.
+    // Nó sẽ bị chuyển hướng bởi OldLoginInterceptor
+    @Deprecated
+    @RequestMapping(value = { "/admin/oldLogin" })
+    public String oldLogin(Model model) {
+   
+        // Code ở đây không bao giờ được chạy.
+   
+        return "oldLogin";
+    }
+   
+    @RequestMapping(value = { "/admin/login" })
+    public String login(Model model) {
+   
+        System.out.println("\n-------- MainController.login --- ");
+   
+        System.out.println(" ** You are in Controller ** ");
+   
+        return "login";
+    }
 }
